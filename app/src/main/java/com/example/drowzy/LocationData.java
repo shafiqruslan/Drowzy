7477aac35c59adaf793a5f6fa713bb32acbdd799
Shafiq Ruslan <budakpadu@gmail.com>
2019-05-12 20:37:45 +0800

    update

---
+++ app/src/main/java/com/example/drowzy/LocationData.java
@@ -0,0 +1,15 @@
+package com.example.drowzy;
+
+public class LocationData{
+    public String name;
+    public double latitude;
+    public double longitude;
+    public double distance;
+
+    public LocationData(String name,double latitude,double longitude,double distance){
+        this.name=name;
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.distance=distance;
+    }
+}
